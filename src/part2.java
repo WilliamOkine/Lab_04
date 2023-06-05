e47cc12df6b34f0b6c7af932c2086172832f9c60
WilliamOkine <okinewn@mail.uc.edu>
2023-06-05 12:15:35 -0400

    commit1

---
+++ src/part2.java
@@ -0,0 +1,21 @@
+public class part2 {
+    public static void main(String[] args) {
+        int lunch = 15;
+        int siblings = 3;
+        String raining = "no";
+        double gas = 3.37;
+        int favNumber = 7;
+        double shoeSize = 11.5;
+        int birthMonth = 12;
+        String fullName = "William Okine";
+
+        System.out.println("The cost of lunch is" + lunch);
+        System.out.println("The amount of siblings I have are " + siblings);
+        System.out.println("Will it rain today? " + raining);
+        System.out.println("The cost of gas is" + gas);
+        System.out.println("What is your favorite number? " + favNumber);
+        System.out.println("What is your shoe size? " + shoeSize);
+        System.out.println("What is your birth month? " + birthMonth);
+        System.out.println("What is your name? " + fullName);
+    }
+}
